b805fdf1e6a99df091ecc5fdec13336f1437724a
DG <sand_king1@mail.ru>
2023-08-26 15:15:17 +0300

    starting stage6

--- stage4/script.js
+++ stage4/script.js
@@ -1,3 +1,8 @@
-<scrip>
-    
-</scrip>
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.heigth = 700;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(50, 50, 50, 50);
